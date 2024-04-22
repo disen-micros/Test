c30c22fccdf9ba8e28acbdd94b7c378255c1848e
disen-micros <jose.pizano@cinvestav.mx>
2024-04-22 10:11:31 -0600

    Sin comentarios

--- Debug/source/subdir.mk
+++ Debug/source/subdir.mk
@@ -4,15 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../source/Test.c \
+../source/main.c \
 ../source/semihost_hardfault.c 
 
 OBJS += \
-./source/Test.o \
+./source/main.o \
 ./source/semihost_hardfault.o 
 
 C_DEPS += \
-./source/Test.d \
+./source/main.d \
 ./source/semihost_hardfault.d 
 
 
